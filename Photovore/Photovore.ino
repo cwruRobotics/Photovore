55f47a57180602d9619d315651b4e91af934ff50
Marcus Daly <mrd109@case.edu>
2016-10-06 19:22:17 -0400

    Panels update and setup for experimental values
    Updates optimalInclination to move panels. Makes delay times fields for
    experimental values need to test for.

--- Photovore/Photovore.ino
+++ Photovore/Photovore.ino
@@ -16,19 +16,21 @@ int stablePosition = 0;     // variable to determine if the position of the head
 int lengthOfStability = 0;    // variable to store time since the head last moved
 int panelLB = 45; //lower bound on rotation of Panel
 int panelUB = 90; //upper bound on rotation of Panel
+int panelTurnTime = 15; // in ms per degree amount -- test for this!
+int headTurnTime = 15; // in ms per degree amount -- test for this!
 int panelVoltagePin = -1; //what should this be (analog)
 int panelVoltage;
-int someVoltage = 10;
+int someVoltage = 10; // variable to store voltage threshold -- test for this!
 bool stable = false;
 
 void setup() {
   // put your setup code here, to run once:
   Head.attach(9);  // attaches the servo on pin 9 to the servo Head object
   Panels.attach(10); // attaches the servo on pin 10 to the servo Panels object
-  pinMode(1, OUTPUT); // left motor forward
-  pinMode(2, OUTPUT); // left motor backward
-  pinMode(3, OUTPUT); // right motor forward
-  pinMode(4, OUTPUT); // right motor backward
+  pinMode(4, OUTPUT); // left motor forward
+  pinMode(5, OUTPUT); // left motor backward
+  pinMode(2, OUTPUT); // right motor forward
+  pinMode(3, OUTPUT); // right motor backward
   posPanels = panelLB;
   Panels.write(posPanels);
 }
@@ -59,17 +61,24 @@ void loop() {
 */
 
 // my attempt at rewriting stuff
-  findOptimalRotation(33);
-  rotateRobot(33);
-  panelVoltage = analogRead(panelVoltagePin);
-  if (panelVoltage < someVoltage) moveRobotAntiBackwards(1000);//move forward for a bit i think
-  if (!stable && panelVoltage >= someVoltage) {
-    posPanels = optimalInclination();
-    Panels.write(posPanels);
-    stable=true;
-  }else if (panelVoltage >= someVoltage){
+  if (!stable) {
+  
+    findOptimalRotation(33);
+    rotateRobot(33);
+    panelVoltage = analogRead(panelVoltagePin);
+//    panelVoltage = 5;
+    if (panelVoltage < someVoltage) moveRobotAntiBackward(1000L);//move forward for a bit i think
+    if (panelVoltage >= someVoltage) {
+      posPanels = optimalInclination();
+      Panels.write(posPanels);
+      stable = true;
+  } else if (panelVoltage >= someVoltage){
     //wait and sit and do nothing
     delay(123456789L);
+  } else {
+    //Reorient to find voltage
+    stable = false;
+    }
   }
 }
 
@@ -79,45 +88,45 @@ boolean approximatelyEqual(int val1, int val2, int threshold) {
 
 void turnRobotRight(long milliseconds) { // turns on the left motor forward for the given number of milliseconds
   /*first ensure all motors are off to avoid shorting*/
-  digitalWrite(1, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
   digitalWrite(2, LOW);
   digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
 
-  digitalWrite(1, HIGH);
+  digitalWrite(4, HIGH);
   delay(milliseconds);
-  digitalWrite(1, LOW);
+  digitalWrite(4, LOW);
 }
 
 void turnRobotLeft(long milliseconds) { // turns on the right motor forward for the given number of milliseconds
   /*first ensure all motors are off to avoid shorting*/
-  digitalWrite(1, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
   digitalWrite(2, LOW);
   digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
 
-  digitalWrite(3, HIGH);
+  digitalWrite(2, HIGH);
   delay(milliseconds);
-  digitalWrite(3, LOW);
+  digitalWrite(2, LOW);
 }
 
 void moveRobotAntiBackward(long milliseconds){
-  digitalWrite(1, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
   digitalWrite(2, LOW);
   digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
 
-  digitalWrite(3, HIGH);
-  digitalWrite(1, HIGH);
+  digitalWrite(2, HIGH);
+  digitalWrite(4, HIGH);
   delay(milliseconds);
-  digitalWrite(3, LOW);
-  digitalWrite(1, LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(4, LOW);
 }
 
 //find optimal direction to travel in and rotate head to that position
 void findOptimalRotation(int threshold) { 
   sensorValueLeft = analogRead(sensorPinLeft);    // read off the voltage from the left sensor (increased light intensity -> smaller values)
-  sensorValueRight = analogRead(sensorPinRight);    // read off the voltage from the rgiht sensor (increased light intensity -> smaller values)
+  sensorValueRight = analogRead(sensorPinRight);    // read off the voltage from the right sensor (increased light intensity -> smaller values)
   while (!approximatelyEqual(sensorValueLeft, sensorValueRight, threshold)) {
     if (sensorValueLeft < sensorValueRight) {   // if there is stronger light intensity to the left of the head rotate left 1 degree
       posHead -= 1;
@@ -125,14 +134,14 @@ void findOptimalRotation(int threshold) {
         posHead = 0;
       }
       Head.write(posHead);
-      delay(15);    // waits 15ms for the servo to reach the position
+      delay(headTurnTime);    // waits 15ms for the servo to reach the position
     } else if (sensorValueLeft > sensorValueRight) {   // if there is stronger light intensity to the right of the head rotate right 1 degree
       posHead += 1;
       if (posHead > 180) { // the angle does not overrun 180 degrees
         posHead = 180;
       }
       Head.write(posHead);
-      delay(15);    // waits 15ms for the servo to reach the position
+      delay(headTurnTime);    // waits 15ms for the servo to reach the position
     }
     sensorValueLeft = analogRead(sensorPinLeft);    // read off the voltage from the left sensor (increased light intensity -> smaller values)
     sensorValueRight = analogRead(sensorPinRight);    // read off the voltage from the rgiht sensor (increased light intensity -> smaller values)
@@ -140,11 +149,19 @@ void findOptimalRotation(int threshold) {
 }
 
 int optimalInclination() {
-  int bestAngle = panelNow;
+  int bestAngle = posPanels;
   int bestVoltage = analogRead(panelVoltagePin);
-  for (int i = panelLB; i < panelUB; i++) {
+  Panels.write(panelLB);
+  delay(panelTurnTime * (posPanels - panelLB));
+  for (int i = panelLB; i <= panelUB; i++) {
+    Panels.write(i);
+    delay(panelTurnTime);
     int nowVoltage = analogRead(panelVoltagePin);
-    if (nowVoltage > bestVoltage) bestAngle = i; //current maximum
+    if (nowVoltage > bestVoltage) {
+      bestAngle = i; //current maximum
+      bestVoltage = nowVoltage;
+    }
+    
   }
   return bestAngle;
 }
@@ -152,15 +169,17 @@ int optimalInclination() {
 void rotateRobot(int threshold) {
   int goalSensors = (sensorValueLeft + sensorValueRight);
   int goalPos = posHead;
+  int robotTurnTime = 100; // placeholder for how long to turn
   while (abs(posHead - 90) > 3) {
     if (posHead > 90) {
-      turnRobotRight(100); //we should make this time a function of abs(posHead-90) but need to experiment
+      turnRobotRight(robotTurnTime); //we should make this time a function of abs(posHead-90) but need to experiment
       findOptimalRotation(threshold);
     } else if (posHead < 90) {
-      turnRobotLeft(100);
+      turnRobotLeft(robotTurnTime);
       findOptimalRotation(threshold);
     }
   }
 }
+
 
 
